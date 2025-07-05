19edbfd0b0a658aee8fb854193124b599586d2f3
Thelostfy <1arian.salehi1389@gmail.com>
2025-07-05 15:56:27 +0330

    Add files via upload

---
+++ tamrin/tamrin.js
@@ -0,0 +1,61 @@
+// function CreateFullName(name1,name2) {
+//     console.log(name1 + name2);
+
+// }
+
+// CreateFullName(`arian`+" "+ `salehi`);
+
+// nmidoonam chra akharesh undefined miare
+
+// --------------------tamrin 2
+// function email(name){
+//     if(name.includes("@"))
+//         console.log(true);
+//     else
+//         console.log(false);
+
+        
+// }
+// email('hassanQ@gmale.com')
+// email('hassanQ_male.com')
+
+// gol kashtam :)
+
+// -------------------tamrin 3
+
+// function getFirstThreeChars(name ) {
+//      console.log(`${name}`.slice(0 , 3));
+    
+// }
+// getFirstThreeChars(`javascript`)
+
+// ------------------------------tamrin 4
+
+// function replaceBadWithGood(jomle){
+//      console.log(`${jomle}`.replaceAll(`bad` , `good`));
+     
+
+// }
+// replaceBadWithGood(`this is is a bad day`);
+
+
+// -------------------------tamrin 5
+// function repeatString(name){
+//      console.log(`${name}`.repeat(3));
+     
+// }
+// repeatString(`Hi`)
+// repeatString(`سلام`)
+
+
+// --------------------------tamrin 6
+function getAgeCategory(age){
+     if(age >= 18)
+          console.log`(بزرگسال)`;
+     else 
+     console.log(`نابالغ`);
+     
+          
+}
+getAgeCategory(18)
+getAgeCategory(14)
