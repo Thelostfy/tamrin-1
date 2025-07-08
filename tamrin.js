9a68eb84e7bcbf5dcf5d772010f22cbbbd9d5838
Thelostfy <1arian.salehi1389@gmail.com>
2025-07-08 15:30:02 +0330

    Add files via upload

---
+++ tamrin.js
@@ -0,0 +1,123 @@
+// function CreateFullName(name1,name2) {
+//     console.log(name1 + name2);
+
+// }
+
+// CreateFullName(`arian`+" "+ `salehi`);
+
+// nmidoonam chra akharesh undefined miare
+
+// --------------------tamrin 2
+// function email(name){
+//     if(name.includes("@"))
+//         console.log(true);
+//     else
+//         console.log(false);
+
+        
+// }
+// email('hassanQ@gmale.com')
+// email('hassanQ_male.com')
+
+// gol kashtam :)
+
+// -------------------tamrin 3
+
+// function getFirstThreeChars(name ) {
+//      console.log(`${name}`.slice(0 , 3));
+    
+// }
+// getFirstThreeChars(`javascript`)
+
+// ------------------------------tamrin 4
+
+// function replaceBadWithGood(jomle){
+//      console.log(`${jomle}`.replaceAll(`bad` , `good`));
+     
+
+// }
+// replaceBadWithGood(`this is is a bad day`);
+
+
+// -------------------------tamrin 5
+// function repeatString(name){
+//      console.log(`${name}`.repeat(3));
+     
+// }
+// repeatString(`Hi`)
+// repeatString(`سلام`)
+
+
+// --------------------------tamrin 6
+// function getAgeCategory(age){
+//      if(age >= 18)
+//           console.log`(بزرگسال)`;
+//      else 
+//      console.log(`نابالغ`);
+     
+          
+// }
+// getAgeCategory(18)
+// getAgeCategory(14)
+
+// ----------------------------tamrin 7
+
+// balad nistam
+
+// -----------------------------tamrin 8
+// function checkPasswordStrength(password){
+//     if(password.length >= 8)
+//         console.log(`ghavi`);
+//     else
+//     console.log(`zaiif`);
+    
+        
+    
+// }
+// checkPasswordStrength(`arian133192`)
+// checkPasswordStrength(`batman`)
+
+// -----------------tamrin 9 
+// function checkEvenOdd(number){
+//     if( number % 2 === 0)
+//         console.log(`zoj`);
+//     else 
+//     console.log(`fard`);
+    
+        
+// }
+// checkEvenOdd(5)
+// checkEvenOdd(6)
+
+// ---------------tamrin 10
+
+
+
+// // ------------------ tamrin 11
+// function login(user , password) {
+//     if(user===`admin` && password=== `secret123`)
+//         console.log(`sahih`);
+//     else
+//     console.log(`ghalat`);
+    
+        
+    
+// }
+// login(`user` , `password`)
+// login(`admin` , `secret123`)
+
+
+// -----------------tamrin 12
+
+
+// tamrin 13
+function roozshomar(rooz){
+    if(rooz === `shanbe`|| rooz === `yekshanbe`)
+        console.log(`weekend`);
+    else
+    console.log(`rooz kari`);
+    
+        
+}
+roozshomar(`shanbe`)
+roozshomar(`jome`)
