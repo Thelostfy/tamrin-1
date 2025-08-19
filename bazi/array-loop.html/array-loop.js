24260cbe9b4cda7ad8a7876da78b8244927ceabb
arian salehi <1arian.salehi1389@gmail.com>
2025-08-19 14:25:29 +0330

    nemidoonam vaghan hard bood ya chon maghz man khastas vali dar kol ok

---
+++ bazi/array-loop.html/array-loop.js
@@ -0,0 +1,71 @@
+// let doostan = ["Saeed", "Armin", "Sara", "Niloofar"]
+// let reversed = doostan.map(function(name){
+//     let split = name.split("");
+//     let reverse = split.reverse();
+//     let join =  reverse.join('')
+//     return join;
+// });
+
+// console.log(reversed);
+
+// very hardddd
+
+// tamrin 2
+// let ghahermanha = ["Flash", "Superman", "Ironman", "Hulk", "Spiderman", "Antman", "DoctorStrange"];
+
+
+// function nameLenght6 (name){
+//     ghahermanha.join(" ");
+//    if(name >= name.length === 6){
+//     console.log(true );
+    
+//    } 
+// }
+
+
+// nameLenght6(ghahermanha)
+
+
+
+// khoodii maghzam nemitoone ye bar saai kardam ba for benivasam nshod dg idk
+
+
+
+
+
+// tamrin 3
+
+// let letters = ["a", "b", "c", "d", "e", "f","g","h","i","j"];
+// letters.forEach((element , index) => {
+//     if (index % 2 === 0) {
+//         console.log(`${index} : ${ element}`);
+        
+//     }
+    
+// });
+
+
+
+//motovaset bood sar for gir kardam just
+
+//tamrin 4
+
+// let adadha = [4, 7, 12, 9, 21, 8, 15, 6, 3];
+
+
+// let fard = adadha.filter((num) => num % 2 !== 0);
+// console.log(fard);
+
+// let jamefard = fard.reduce((adadfeli , number) => adadfeli + number, 0)
+// console.log(jamefard);
+
+// motovaset ro ba hard
+
+
+// tamrin 5
+
+let pesarha = ["Ali", "Reza", "Amir"];
+let dokhtarha = ["Sara", "Niloofar", "Mina"];
+
+
+// idk :)))))))))))))
