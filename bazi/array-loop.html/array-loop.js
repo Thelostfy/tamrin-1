df865be31e304317120d4d9ab834cb27e53df678
arian salehi <1arian.salehi1389@gmail.com>
2025-08-26 12:46:36 +0330

    khedmat shoma

--- bazi/array-loop.html/array-loop.js
+++ bazi/array-loop.html/array-loop.js
@@ -1,12 +1,28 @@
-// let doostan = ["Saeed", "Armin", "Sara", "Niloofar"]
-// let reversed = doostan.map(function(name){
-//     let split = name.split("");
-//     let reverse = split.reverse();
-//     let join =  reverse.join('')
-//     return join;
-// });
 
-// console.log(reversed);
+// let doostan = ["Saeed", "Armin", "Sara", "Niloofar"]
+
+// function reversekalame(name){
+//     let khiyar = [];
+//     for (let i = 0; i < name.length; i++) {
+//        khiyar.unshift(name[i]);
+        
+//     }
+//         return khiyar.join("");
+//      }
+//      let havij = [];
+//      for (let a = 0; a < doostan.length; a++) {
+//         let x =reversekalame(doostan[a]);
+//         console.log(x); 
+//         havij.push(x);
+//      }
+
+// console.log(havij);
+
+
+
+
+
+
 
 // very hardddd
 
@@ -14,20 +30,33 @@
 // let ghahermanha = ["Flash", "Superman", "Ironman", "Hulk", "Spiderman", "Antman", "DoctorStrange"];
 
 
-// function nameLenght6 (name){
-//     ghahermanha.join(" ");
-//    if(name >= name.length === 6){
-//     console.log(true );
+
+// function length5(name) {
+
+//     if (name.length >= 5) {
+//          return name;
+//           }
+//           else 
+//             return `کوتاهه`;
+
+
     
-//    } 
 // }
+// console.log(length5(ghahermanha));
+// let moz = [];
+// for (let a = 0; a < ghahermanha.length; a++) {
+//     let dokhtari = length5(ghahermanha[a]);
+//     moz.push(dokhtari); 
+    
+// }
+// console.log(moz);
 
 
-// nameLenght6(ghahermanha)
 
 
 
-// khoodii maghzam nemitoone ye bar saai kardam ba for benivasam nshod dg idk
+
+
 
 
 
@@ -36,30 +65,45 @@
 // tamrin 3
 
 // let letters = ["a", "b", "c", "d", "e", "f","g","h","i","j"];
-// letters.forEach((element , index) => {
+// function jaygahzoj(element , index) {
 //     if (index % 2 === 0) {
-//         console.log(`${index} : ${ element}`);
-        
+//         return element;
 //     }
+//     else 
+//         return `نیست`;
     
-// });
+// }
+// let mashti = [];
+// for (let i = 0; i < letters.length; i++) {
+//     let z = jaygahzoj(letters[i], i)
+//     mashti.push(z)
+    
+// }
+// console.log(mashti);
 
 
 
-//motovaset bood sar for gir kardam just
 
 //tamrin 4
 
-// let adadha = [4, 7, 12, 9, 21, 8, 15, 6, 3];
+let adadha = [4, 7, 12, 9, 21, 8, 15, 6, 3];
+let jamefard = 0;
 
 
-// let fard = adadha.filter((num) => num % 2 !== 0);
-// console.log(fard);
 
-// let jamefard = fard.reduce((adadfeli , number) => adadfeli + number, 0)
-// console.log(jamefard);
 
-// motovaset ro ba hard
+for (let i = 0; i < adadha.length; i++) {
+    if (adadha[i] % 2 !== 0) {
+        console.log(adadha[i]);
+        jamefard += adadha[i]
+    }
+    else
+        console.log();
+         
+}
+console.log(`${jamefard}`);
+
+
 
 
 // tamrin 5
@@ -68,4 +112,4 @@ let pesarha = ["Ali", "Reza", "Amir"];
 let dokhtarha = ["Sara", "Niloofar", "Mina"];
 
 
-// idk :)))))))))))))
+// خدایی نمیدونم
