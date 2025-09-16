3c1d611eea4d65835f41e7f0a3cec7b045be253a
arian salehi <1arian.salehi1389@gmail.com>
2025-09-16 12:38:39 +0330

    beesyar awli bood tamrin ha .thank you

---
+++ bazi/objekt/javascrpit.js
@@ -0,0 +1,96 @@
+// let person = {
+//     name: "Ali",
+//     age: 25,
+//     city: "isfahan"
+// };
+// console.log(person);
+
+// person.age = 18;
+
+// console.log(person);
+
+
+// person.email = "aliisfahani@gmail.com";
+// console.log(person);
+
+
+//------------------------------------
+
+// let stu = {
+//     name: "mmd",
+//     grad: [20,10,15],
+//     address: {
+//         city: "isfahan",
+//         street: "jolfa"
+//     }
+// };
+
+
+// // let x = stu.address
+// console.log(stu.address);
+// stu.address.city = "tabriz";
+// console.log(stu.address);
+
+
+
+
+//----------------------------------------------
+
+
+
+// let book = {
+//     title: "Heir of fire",
+//     author: "Mir Fendereski",
+//     tags: ["Romance","Mystery" ,"Fantasy"]
+
+// }
+
+// book.tags.unshift("Horror");
+// let x = book.tags.length;
+// console.log(x);
+
+
+
+
+//---------------------------------------------------
+
+
+// let cars = [
+//     { brand: "irankhodro",
+//         model: "peride",
+//         year: 97
+//     }
+// ]
+
+// cars.push({brand:"bmw",model:"bmw 520d",year: 1972});
+// let x = cars[1];
+// console.log(x);
+
+
+//-------------------------------------------------------------
+
+
+// let company = {
+//     name: "futcher developer",
+//     employees: [{name:"mohamad",roll:["web developer"],skill:["fast type"]},
+//          {name:"parmida",roll: ["virastar"],skill: ["clever"]},
+//           { name:"sara",roll:["frooshande"],skill:["fane bayan"]}]
+// };
+// company.employees.push({name:"amir",roll: ["sarparast"], skill: ["tahlil dade"]})
+// // console.log(company);
+
+// company.employees[0].skill.push("editor");
+
+
+
+
+// let y = [];
+// for (let i = 0; i < company.employees.length; i++) {
+//     y.push(company.employees[i].name);
+
+// }
+
+// console.log(y);
+
+
+//---------------------------------------
