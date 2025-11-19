3c026a6f731d73ceb0d5911dfa92bd42b5c5e3ed
aryan salehi <1arian.salehi1389@gmail.com>
2025-11-19 18:03:48 +0330

    update js

--- new/Xo/js.js
+++ new/Xo/js.js
@@ -10,7 +10,9 @@ for (const button of buttons) {
 
     button.addEventListener("click", () => {
         // alert(`you click on ${button.value}`)
-        let [satr, soton] = button.value.split('-');
+        if (button.innerText !== "") return;
+
+        let [satr, soton] = button.value.split(',');
         // let satr = [satrarray , sotonarray][0];
         // let soton = [satrarray , sotonarray][1];
         satr = Number(satr);
@@ -30,10 +32,26 @@ for (const button of buttons) {
             button.style.color = `white`;
         }
 
+        if (XO[satr][0] !== "" && XO[satr][0] === XO[satr][1] && XO[satr][1] === XO[satr][2]) {
+            alert(`Player ${button.innerText} wins!`);
+        }
+        if (XO[0][soton] !== "" && XO[0][soton] === XO[1][soton] && XO[1][soton] === XO[2][soton]) {
+            alert(`Player ${button.innerText} wins!`);
+
+        }
+        if (XO[0][0] !== "" && XO[0][0] === XO[1][1] && XO[1][1] === XO[2][2]) {
+            alert(`Player ${button.innerText} wins!`);
+
+        }
+        if (XO[0][2] !== "" && XO[0][2] === XO[1][1] && XO[1][1] === XO[2][0]) {
+            alert(`Player ${button.innerText} wins!`);
+
+        }
 
 
-
+localStorage.setItem("BAZIhayeghabli" , JSON.stringify(XO))
         i++;
+
     })
 
 }
